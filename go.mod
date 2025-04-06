3929979048560a38758a52e5c76f4ee3d84e1344
apstndb <803393+apstndb@users.noreply.github.com>
2025-04-07 04:55:59 +0900

    feature: Add human-readable query plan, and implements include_proto_descriptors

--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.24
 
 require (
 	cloud.google.com/go/spanner v1.78.0
+	github.com/apstndb/lox v0.0.0-20230530141045-98c1efebcde8
+	github.com/apstndb/spannerplanviz v0.3.3
 	github.com/go-viper/mapstructure/v2 v2.2.1
 	github.com/golang/protobuf v1.5.4
 	github.com/mark3labs/mcp-go v0.18.0
+	github.com/olekukonko/tablewriter v0.0.5
+	github.com/samber/lo v1.47.0
 	google.golang.org/protobuf v1.36.6
 )
 
@@ -33,7 +37,10 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
+	github.com/mattn/go-runewidth v0.0.10 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/xlab/treeprint v1.1.0 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
@@ -46,6 +53,7 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.35.0 // indirect
 	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
 	golang.org/x/net v0.37.0 // indirect
 	golang.org/x/oauth2 v0.28.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
@@ -57,4 +65,5 @@ require (
 	google.golang.org/genproto/googleapis/api v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250313205543-e70fdf4c4cb4 // indirect
 	google.golang.org/grpc v1.71.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"cloud.google.com/go/spanner"
 	database "cloud.google.com/go/spanner/admin/database/apiv1"
 	"cloud.google.com/go/spanner/admin/database/apiv1/databasepb"
+	"github.com/apstndb/lox"
 	"github.com/go-viper/mapstructure/v2"
 	"github.com/golang/protobuf/proto"
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
+	"github.com/olekukonko/tablewriter"
+	"github.com/samber/lo"
 	"google.golang.org/protobuf/encoding/prototext"
 	"google.golang.org/protobuf/types/descriptorpb"
+	"github.com/apstndb/spannerplanviz/plantree"
+	"github.com/apstndb/spannerplanviz/queryplan"
 )
 
 func mapToStruct[T any](m map[string]any) (T, error) {
@@ -33,7 +39,7 @@ func main() {
 
 	// Add tool
 	plan := mcp.NewTool("plan",
-		mcp.WithDescription("Get execution plan for the query"),
+		mcp.WithDescription("Get execution plan for the query. The first content is machine-readable prototext format of QueryPlan message. The second content is human-readable rendered query plan."),
 		mcp.WithString("query",
 			mcp.Required(),
 			mcp.Description("query text of SQL or GQL"),
@@ -53,7 +59,7 @@ func main() {
 	)
 
 	getDDL := mcp.NewTool("get_ddl",
-		mcp.WithDescription("Get DDL of the database. The first content is the whole response, and the second content is unmarshalled proto_descriptors."),
+		mcp.WithDescription("Get DDL of the database. The first content is the whole response, and the second content is unmarshalled proto_descriptors (optional)."),
 		mcp.WithString("project",
 			mcp.Required(),
 			mcp.Description("Google Cloud project"),
@@ -65,6 +71,10 @@ func main() {
 		mcp.WithString("database",
 			mcp.Required(),
 			mcp.Description("Spanner database id"),
+		),
+		mcp.WithBoolean("include_proto_descriptors",
+			mcp.DefaultBool(false),
+			mcp.Description("Enable only if proto_descriptors is needed."),
 		),
 	)
 
@@ -117,14 +127,32 @@ func planHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToo
 	if err != nil {
 		return nil, err
 	}
-	return mcp.NewToolResultText(prototext.Format(qp)), nil
+
+	plan := queryplan.New(qp.GetPlanNodes())
+	processed, err := plantree.ProcessPlan(plan)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := printResult(processed)
+	if err != nil {
+		return nil, err
+	}
+
+	return &mcp.CallToolResult{
+		Content: []mcp.Content{
+			mcp.NewTextContent(prototext.Format(qp)),
+			mcp.NewTextContent(result),
+		},
+	}, nil
 }
 
 func getDDLHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
 	req, err := mapToStruct[struct {
-		Project  string
-		Instance string
-		Database string
+		Project                 string
+		Instance                string
+		Database                string
+		IncludeProtoDescriptors bool
 	}](request.Params.Arguments)
 	if err != nil {
 		return nil, err
@@ -148,11 +176,18 @@ func getDDLHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallT
 		return nil, err
 	}
 
+	var contents []mcp.Content
+
+	if req.IncludeProtoDescriptors {
+		contents = append(contents, mcp.NewTextContent(prototext.Format(resp)))
+		contents = append(contents, mcp.NewTextContent(prototext.Format(&fds)))
+	} else {
+		resp.ProtoDescriptors = nil
+		contents = append(contents, mcp.NewTextContent(prototext.Format(resp)))
+	}
+
 	return &mcp.CallToolResult{
-		Content: []mcp.Content{
-			mcp.NewTextContent(prototext.Format(resp)),
-			mcp.NewTextContent(prototext.Format(&fds)),
-		},
+		Content: contents,
 	}, nil
 }
 
@@ -195,4 +230,78 @@ func updateDDLHandler(ctx context.Context, request mcp.CallToolRequest) (*mcp.Ca
 
 func databasePath(project string, instance string, database string) string {
 	return fmt.Sprintf("projects/%s/instances/%s/databases/%s", project, instance, database)
+}
+
+func printResult(rows []plantree.RowWithPredicates) (string, error) {
+	var b strings.Builder
+	table := tablewriter.NewWriter(&b)
+	table.SetAutoFormatHeaders(false)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	table.SetColumnAlignment([]int{tablewriter.ALIGN_RIGHT, tablewriter.ALIGN_LEFT})
+	table.SetAutoWrapText(false)
+
+	for _, row := range rows {
+		table.Append([]string{row.FormatID(), row.Text()})
+	}
+	table.SetHeader([]string{"ID", "Operator"})
+	if len(rows) > 0 {
+		table.Render()
+	}
+
+	var maxIDLength int
+	for _, row := range rows {
+		if length := len(fmt.Sprint(row.ID)); length > maxIDLength {
+			maxIDLength = length
+		}
+	}
+
+	var predicates []string
+	var parameters []string
+	for _, row := range rows {
+		var prefix string
+		for i, predicate := range row.Predicates {
+			if i == 0 {
+				prefix = fmt.Sprintf("%*d:", maxIDLength, row.ID)
+			} else {
+				prefix = strings.Repeat(" ", maxIDLength+1)
+			}
+			predicates = append(predicates, fmt.Sprintf("%s %s", prefix, predicate))
+		}
+
+		i := 0
+		for _, t := range lox.EntriesSortedByKey(row.ChildLinks) {
+			typ, childLinks := t.Key, t.Value
+			if typ == "" {
+				continue
+			}
+
+			if i == 0 {
+				prefix = fmt.Sprintf("%*d:", maxIDLength, row.ID)
+			} else {
+				prefix = strings.Repeat(" ", maxIDLength+1)
+			}
+
+			join := strings.Join(lo.Map(childLinks, func(item *queryplan.ResolvedChildLink, index int) string {
+				if varName := item.ChildLink.GetVariable(); varName != "" {
+					return fmt.Sprintf("$%s=%s", item.ChildLink.GetVariable(), item.Child.GetShortRepresentation().GetDescription())
+				} else {
+					return item.Child.GetShortRepresentation().GetDescription()
+				}
+			}), ", ")
+			if join == "" {
+				continue
+			}
+			i++
+			typePartStr := lo.Ternary(typ != "", typ+": ", "")
+			parameters = append(parameters, fmt.Sprintf("%s %s%s", prefix, typePartStr, join))
+		}
+	}
+
+	if len(predicates) > 0 {
+		b.WriteString("Predicates(identified by ID):\n")
+		for _, s := range predicates {
+			b.WriteString(fmt.Sprintf(" %s\n", s))
+		}
+	}
+	return b.String(), nil
 }
